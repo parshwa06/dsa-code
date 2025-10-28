46861358a310f64a6ec4c24fc23e934c85916633
parshwa06 <parshwakansara.06@gmail.com>
2025-10-28 13:50:52 +0530

    Adding code of DSA in python

---
+++ python/binaryTree.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if not self.root:
+            self.root = Node(value)
+        else:
+            self._insert(self.root, value)
+
+    def _insert(self, current, value):
+        if value < current.value:
+            if current.left is None:
+                current.left = Node(value)
+            else:
+                self._insert(current.left, value)
+        else:
+            if current.right is None:
+                current.right = Node(value)
+            else:
+                self._insert(current.right, value)
+
+    def inorder(self):
+        self._inorder(self.root)
+        print()
+
+    def _inorder(self, node):
+        if node:
+            self._inorder(node.left)
+            print(node.value, end=" ")
+            self._inorder(node.right)
+
+if __name__ == "__main__":
+    tree = BinaryTree()
+    values = [10, 5, 15, 2, 7, 12, 20]
+    for v in values:
+        tree.insert(v)
+    print("Inorder traversal:")
+    tree.inorder()
